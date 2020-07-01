68d1b5e4b81ef3d4fb48c956af5f49ef1e4a1a87
LukePowe <ltpowe@googlemail.com>
2020-07-01 17:39:11 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,182 @@
+#include "Includes.h"
+#include "VertexData.h"
+
+// Function prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+
+
+// Camera settings
+//							  width, heigh, near plane, far plane
+Camera_settings camera_settings{ 1000, 800, 0.1, 100.0 };
+
+//Timer
+Timer timer;
+
+// Instantiate the camera object with basic data
+Camera camera(camera_settings);
+
+
+double lastX = camera_settings.screenWidth / 2.0f;
+double lastY = camera_settings.screenHeight / 2.0f;
+
+int main()
+{
+	// glfw: initialize and configure
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	// glfw window creation
+	GLFWwindow* window = glfwCreateWindow(camera_settings.screenWidth, camera_settings.screenHeight, "Real-Time Rendering: Tutorial 6", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Set the callback functions
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	// glad: load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Rendering settings
+	glfwSwapInterval(1);		// glfw enable swap interval to match screen v-sync
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE); //Enables face culling
+	glFrontFace(GL_CCW);//Specifies which winding order if front facing
+
+	PrincipleAxes	*principleAxes = new PrincipleAxes();
+
+	////	Shaders - Textures - Models	////
+	GLuint phongShader;
+
+	GLSL_ERROR glsl_err = ShaderLoader::createShaderProgram(
+		string("Resources\\Shaders\\Phong_shader.vs"),
+		string("Resources\\Shaders\\Phong-texture.gs"),
+		string("Resources\\Shaders\\Phong_shader.fs"),
+		&phongShader);
+
+	
+
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pointData), &pointData, GL_STATIC_DRAW);
+
+	GLint posAttrib = glGetAttribLocation(phongShader, "pos");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+
+	GLint colAttrib = glGetAttribLocation(phongShader, "colour");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(2* sizeof(float)));
+
+	GLint sidesAttrib = glGetAttribLocation(phongShader, "sides");
+	glEnableVertexAttribArray(sidesAttrib);
+	glVertexAttribPointer(sidesAttrib, 1, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(5 * sizeof(float)));		
+	glEnableVertexAttribArray(posAttrib);
+	
+	//glEnableVertexAttribArray(0);
+	//
+	//glBindVertexArray(0);
+
+///////////////////////////////////////////////////////////
+
+	
+
+	
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+		timer.tick();
+
+
+		// render
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glDrawArrays(GL_POINTS, 0, 4);
+		
+
+
+
+		glUseProgram(phongShader);
+		glBindVertexArray(VAO);
+		
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window)
+{
+	timer.updateDeltaTime();
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard(FORWARD, timer.getDeltaTimeSeconds());
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(BACKWARD, timer.getDeltaTimeSeconds());
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard(LEFT, timer.getDeltaTimeSeconds());
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard(RIGHT, timer.getDeltaTimeSeconds());
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	glViewport(0, 0, width, height);
+	camera.updateScreenSize(width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	double xoffset = xpos - lastX;
+	double yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+		camera.processMouseMovement(xoffset, yoffset);
+	}
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.processMouseScroll(yoffset);
+}
